59a0558b350f112f0e4ad021570e73728dc1fb4d
Sean McIntyre <s.mcintyre@xverba.ca>
2015-10-15 17:27:32 -0400

    Further exploratory work on Pipeline.

--- src/main/scala/software/uncharted/sparkpipe/Pipeline.scala
+++ src/main/scala/software/uncharted/sparkpipe/Pipeline.scala
@@ -1,31 +1,50 @@
-
+package software.uncharted.sparkpipe
 import scala.collection.mutable.ArrayBuffer
 
 /**
  *
  *
  */
-object Pipeline {
-  def op[I, O](opFunc: I => O): PipelineOp[I, O] = {
-    new PipelineOp(opFunc)
+class Pipeline {
+  var root: PipelineStage[_,_] = _
+
+  def start[I, O](opFunc: I => O): PipelineStage[I, O] = {
+    //TODO prevent double creation of root with exception
+    val result = new PipelineStage[I, O](opFunc, None, this)
+    root = result
+    result
+  }
+
+  def run[I](in : I): Unit = {
+    root.asInstanceOf[PipelineStage[I,_]].runStage(in)
   }
 }
 
-class PipelineOp[I, O](
-  opFunc: I => O
-) {
-  val children = new ArrayBuffer[PipelineOp[O,_]]
+class PipelineStage[I, O] (opFunc: I => O, parent: Option[PipelineStage[_, I]], pipe: Pipeline) {
+  val children = new ArrayBuffer[PipelineStage[O,_]]
 
-  def next[X](opFunc: O => X): PipelineOp[O,X] = {
-    val op = new PipelineOp[O,X](opFunc)
-    children.append(op)
-    op
+  def next[X](opFunc: O => X): PipelineStage[O,X] = {
+    val result = new PipelineStage[O,X](opFunc, Some(this), pipe)
+    children.append(result)
+    result
   }
 
-  def run(in: I): Unit = {
+  def apply[Y](in: Y): O = {
+    if (parent.isDefined) {
+      opFunc(parent.get.apply(in))
+    } else {
+      opFunc.asInstanceOf[(Y) => O](in)
+    }
+  }
+
+  def run[Y](in: Y): Unit = {
+    pipe.run(in)
+  }
+
+  private[sparkpipe] def runStage(in: I): Unit = {
     val out: O = opFunc(in)
     children.toArray.foreach(f => {
-      f.run(out)
+      f.runStage(out)
     })
   }
 }
---
+++ src/test/scala/software/uncharted/sparkpipe/Main.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015 Uncharted Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package software.uncharted.sparkpipe
+
+import org.scalatest.tools.Runner
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object Spark {
+  val conf = new SparkConf().setAppName("spark-pipeline")
+  val sc = new SparkContext(conf)
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val testResult = Runner.run(Array("-o", "-R", "build/classes/test"))
+    if (!testResult) {
+      System.exit(1)
+    }
+  }
+}
