141c45e78ca2b3fa983beff39f11331ec21743f2
Sean McIntyre <s.mcintyre@xverba.ca>
2015-11-27 14:37:42 -0500

    Removing unnecessary entrypoint argument from docker commands

--- test-environment.sh
+++ test-environment.sh
@@ -5,6 +5,5 @@ docker run \
 -v $(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
 -v $(pwd):/opt/pipeline \
 -it \
---entrypoint="/startup.sh" \
 --workdir="/opt/pipeline" \
 uncharted/sparklet:1.5.1 bash
