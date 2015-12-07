a7f50d1a0b7924bcd508f77b20e51b0c15aa8262
Sean McIntyre <s.mcintyre@xverba.ca>
2015-12-07 11:21:14 -0500

    Only exposing necessary ports in test-environment.sh [skip ci]

--- test-environment.sh
+++ test-environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 docker run \
---net="host" \
+-p 8080:8080 \
+-p 9999:9999 \
 -e GRADLE_OPTS="-Dorg.gradle.daemon=true" \
--v $(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
--v $(pwd):/opt/pipeline \
+-v /$(pwd)/src/test/resources/log4j.properties:/usr/local/spark/conf/log4j.properties \
+-v /$(pwd):/opt/pipeline \
 -it \
 --workdir="/opt/pipeline" \
 uncharted/sparklet:1.5.2 bash
