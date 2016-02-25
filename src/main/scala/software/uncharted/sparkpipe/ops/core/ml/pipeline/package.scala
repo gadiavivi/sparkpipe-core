630a240b035c83b32d4fffeaabd93d7e797a7a2d
Sean McIntyre <s.mcintyre@xverba.ca>
2016-02-24 19:28:51 -0500

    Documenting new ml ops [skip ci]

--- src/main/scala/software/uncharted/sparkpipe/ops/core/ml/pipeline/package.scala
+++ src/main/scala/software/uncharted/sparkpipe/ops/core/ml/pipeline/package.scala
@@ -27,24 +27,55 @@ import org.apache.spark.ml.{Pipeline => MLPipeline, PipelineStage => MLPipelineS
  */
 package object pipeline {
 
+  /**
+   * Load a spark.ml Pipeline from a file
+   * @param path the path to the persisted Pipeline
+   * @return a spark.ml Pipeline constructed from the given file
+   */
   def load(path: String): MLPipeline = {
     MLPipeline.load(path)
   }
 
+  /**
+   * Persist a spark.ml Pipeline to a file
+   * @param path the path for the persisted Pipeline file
+   * @return the input spark.ml Pipeline, unchanged
+   */
   def save(path: String)(mlpipe: MLPipeline): MLPipeline = {
     mlpipe.save(path)
     mlpipe
   }
 
+  /**
+   * Add a stage (an Estimator or a Transformer) to an existing spark.ml Pipeline
+   * @param stage a spark.ml PipelineStage (an Estimator or Transformer)
+   * @return the input Pipeline, with the added stage
+   */
   def addStage(stage: MLPipelineStage)(mlpipe: MLPipeline): MLPipeline = {
     mlpipe.setStages(mlpipe.getStages ++ Array(stage))
     mlpipe
   }
 
+  /**
+   * Fit a spark.ml pipeline to a DataFrame to produce a PipelineModel.
+   * This op is intended to be used after merging two Pipes, one providng
+   * the spark.ml Pipeline, and the other providing the DataFrame.
+   * For example, Pipe(mlPipelinePipe, dataPipe).to(ops.core.ml.fit)
+   * @param args a Tuple2 including a spark.ml Pipeline and a DataFrame
+   * @return the fitted spark.ml PipelineModel
+   */
   def fit(args: (MLPipeline, DataFrame)): MLPipelineModel = {
     args._1.fit(args._2)
   }
 
+  /**
+   * Apply a spark.ml PipelineModel to a DataFrame to make a prediction.
+   * This op is intended to be used after merging two Pipes, one providng
+   * the spark.ml PipelineModel, and the other providing the DataFrame.
+   * For example, Pipe(mlModelPipe, dataPipe).to(ops.core.ml.applyModel)
+   * @param args a Tuple2 including a spark.ml PipelineModel and a DataFrame
+   * @return the resultant DataFrame
+   */
   def applyModel(args: (MLPipelineModel, DataFrame)): DataFrame = {
     args._1.transform(args._2)
   }
